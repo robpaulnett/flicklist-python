0da7e797945bf5ae29267a751f70a6bdd7c238fb
Paul <paulrobnett@hotmail.com>
2016-08-15 20:32:44 -0500

    implemented TODOS 1 - 3, with 4 pending

--- main.py
+++ main.py
@@ -42,13 +42,30 @@ class Index(webapp2.RequestHandler):
         # TODO 1
         # Include another form so the user can "cross off" a movie from their list.
 
+        crossOff_form = """
+        <form action="/cross-off" method="post">
+            <label>
+                I want to cross-off
+                <input type="text" name="watched-movie" />
+                from my watchlist.
+            </label>
+            <input type="submit" value="remove-it" />
+        </form>
+        """
+        # try delete list as a dropdown instead
+        # <select>
+        #   <option value="volvo">Volvo</option>
+        #   <option value="saab">Saab</option>
+        #   <option value="mercedes">Mercedes</option>
+        #   <option value="audi">Audi</option>
+        # </select>
 
         # TODO 4 (Extra Credit)
         # modify your form to use a dropdown (<select>) instead a
         # text box (<input type="text"/>)
 
 
-        response = page_header + edit_header + add_form + page_footer
+        response = page_header + edit_header + add_form + crossOff_form + page_footer
         self.response.write(response)
 
 
@@ -73,12 +90,26 @@ class AddMovie(webapp2.RequestHandler):
 # Create a new RequestHandler class called CrossOffMovie, to receive and
 # handle the request from your 'cross-off' form. The user should see a message like:
 # "Star Wars has been crossed off your watchlist".
+class CrossOffMovie(webapp2.RequestHandler):
+    """ Handles request coming into /cross-off
+        e.g. www.flicklist.com/cross-off
+    """
+    def post(self):
+        # look inside the request to figure out what the user typed
+        watched_movie = self.request.get("watched-movie")
 
+        # build response content
+        watched_movie_element = "<strong>" + watched_movie + "</strong>"
+        sentence = "<strike>" + watched_movie_element + "</strike>" + " has been crossed off your Watchlist!"
+
+        response = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(response)
 
 
 # TODO 3
 # Include a route for your cross-off handler, by adding another tuple to the list below.
 app = webapp2.WSGIApplication([
     ('/', Index),
-    ('/add', AddMovie)
+    ('/add', AddMovie),
+    ('/cross-off', CrossOffMovie)
 ], debug=True)
